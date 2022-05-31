01cc26adb05df33d1d9d5d4f96e5dc52d28767a2
zach.burnaby <58493767+TheRedLancer@users.noreply.github.com>
2022-05-31 16:39:32 -0700

    add car spawn points

--- Assets/Scripts/VehicleSpawner.cs
+++ Assets/Scripts/VehicleSpawner.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class VehicleSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
         
     }
 }
